9c266863f3725fb6c17212fc1e7bdbed29b65f7c
imTHAI <36070606+imTHAI@users.noreply.github.com>
2018-09-13 20:22:46 +0200

    Update update_dnsmasq_adblock_V2.sh

--- files/update_dnsmasq_adblock_V2.sh
+++ files/update_dnsmasq_adblock_V2.sh
@@ -11,7 +11,7 @@ wget https://raw.githubusercontent.com/notracking/hosts-blocklists/master/hostna
 wget https://raw.githubusercontent.com/notracking/hosts-blocklists/master/domains.txt
 
 
-# Correct & Modify:
+# Correct & Modify ( syntax for compatibility with GNU and Mac sed):
 sed -i.bak -e "/::/d" domains.txt hostnames.txt
 sed -i.bak -e "s/0.0.0.0/$cible/g" domains.txt hostnames.txt
 
@@ -22,11 +22,3 @@ for domain in `cat whitelist` ; do sed -i "/$domain/d" *.txt ; done
 # Add some domains
 for domain in `cat myadd` ; do echo "address=/$domain/$cible" >> domains.txt ; done
 
-
-#Mettre a jour le fichier hosts partagé par python:
-#echo "Update du fichier partagé par python pour Adaway"
-#pkill -f "SCREEN -d -m python3 -m http.server"
-#cp /etc/dnsmasq_hostnames ~/macOS/adblock/share/hosts
-#cd /home/trickyboy/macOS/adblock/share
-#sed -i 's/192.168.12.3/127.0.0.1/g' hosts
-#screen -d -m python3 -m http.server
