20a5d00c242c6ab4d11ada15adcd90614344dcae
imTHAI <36070606+imTHAI@users.noreply.github.com>
2018-09-15 11:34:11 +0200

    Update update_dnsmasq_adblock.sh

--- files/update_dnsmasq_adblock.sh
+++ files/update_dnsmasq_adblock.sh
@@ -6,6 +6,7 @@ cible=192.168.0.1
 # Download the list of domains and the list of hostnames maintained by 
 # https://github.com/notracking/hosts-blocklists
 #
+echo "Applying some corrections to the lists...
 rm -rf domains.txt hostnames.txt
 wget https://raw.githubusercontent.com/notracking/hosts-blocklists/master/hostnames.txt
 wget https://raw.githubusercontent.com/notracking/hosts-blocklists/master/domains.txt
@@ -16,7 +17,6 @@ sed -i.bak -e "/::/d" domains.txt hostnames.txt
 sed -i.bak -e "s/0.0.0.0/$cible/g" domains.txt hostnames.txt
 
 # Whitelist of some domains:
-echo "To whitelist some domains.."
 for domain in `cat whitelist` ; do sed -i.bak "/$domain/d" *.txt ; done
 
 # Add some domains
