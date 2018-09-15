c5ad10c780028f66585325c63f0b731b813d43cd
imTHAI <36070606+imTHAI@users.noreply.github.com>
2018-09-15 11:46:38 +0200

    Update update_dnsmasq_adblock.sh

--- files/update_dnsmasq_adblock.sh
+++ files/update_dnsmasq_adblock.sh
@@ -28,6 +28,6 @@ for domain in `cat whitelist` ; do sed -i.bak "/$domain/d" *.txt ; done
 for domain in `cat myadd` ; do echo "address=/$domain/$cible" >> domains.txt ; done
 
 # Restart the docker
-echo "Restarting qdblock-dnsmasq..."
+echo "Restarting adblock-dnsmasq..."
 docker container restart dns-adblock
 
