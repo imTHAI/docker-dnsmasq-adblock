99b591a9f7d1d66fa4ca832740cb760c146ec5fc
imTHAI <36070606+imTHAI@users.noreply.github.com>
2019-02-02 17:03:23 +0100

    Basically cosmetic changes

--- files/update_adblocklist.sh
+++ files/update_adblocklist.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# You have to put this script inside the same folder where you mounted /adblock from the container 
 
 # If you have a pixelserv server u want to redirect the ads to, define the ip of the server:
 # Default is 0.0.0.0
@@ -8,29 +9,23 @@ cible=0.0.0.0
 # Download the list of domains and the list of hostnames maintained by 
 # https://github.com/notracking/hosts-blocklists
 #
-echo "Applying some corrections to the lists..."
-rm -rf domains.txt hostnames.txt
-wget https://raw.githubusercontent.com/notracking/hosts-blocklists/master/hostnames.txt
-wget https://raw.githubusercontent.com/notracking/hosts-blocklists/master/domains.txt
+wget -q https://raw.githubusercontent.com/notracking/hosts-blocklists/master/hostnames.txt -O hostnames.txt
+wget -q https://raw.githubusercontent.com/notracking/hosts-blocklists/master/domains.txt -O domains.txt
 
-# For security, if downloads failed:
-touch domains.txt hostnames.txt
+# Remove ipv6 stuff
+sed -i.bak -e "/::/d" domains.txt hostnames.txt
 
-# Correct & Modify in case you don't use the files as they are::
-if [[ $cible != "0.0.0.0" ]]
-then
-        sed -i.bak -e "/::/d" domains.txt hostnames.txt
-        sed -i.bak -e "s/0.0.0.0/$cible/g" domains.txt hostnames.txt
-fi
+# Modify in case you don't use the files as they are::
+if [[ $cible != "0.0.0.0" ]] ; then sed -i.bak -e "s/0.0.0.0/$cible/g" domains.txt hostnames.txt ; fi
 
-# Whitelist of some domains:
+# Remove the whitelisted hosts ( the ones we put in the whitelist file) from the lists:
 for host in `cat whitelist`
 do
 domain=$(echo $host | sed "s/.*\.\(.*\..*\)$/\1/")
 sed -i.bak "/$domain/d" *.txt
 done
 
-# Add my own blacklist:
+# Add my own blacklist to the lists:
 for domain in `cat myadd` ; do echo "address=/$domain/$cible" >> domains.txt ; done
 
 # Restart the docker
